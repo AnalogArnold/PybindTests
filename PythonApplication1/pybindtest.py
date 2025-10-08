969fa76ae9b427c46ce4d336687b7e1f0afc1bab
Michael Darcy <45762689+AnalogArnold@users.noreply.github.com>
2025-10-08 15:25:21 +0100

    Pass mesh data to cpp and check that it works

--- PythonApplication1/pybindtest.py
+++ PythonApplication1/pybindtest.py
@@ -1,22 +1,30 @@
+from ast import List
 import numpy as np
 from math import tan, radians
 from dataclasses import dataclass, field
 
-
+'''
 from superfastcode import array_code
 a = array_code(3,4)
 print(a.shape)
-
-
-
 '''
-from pyvale import dataset
+
+def normalise_vector(vector: np.ndarray) -> np.ndarray:
+    '''Returns the normalised vector, i.e., with length 1.0.'''
+    return vector / np.sqrt(vector.dot(vector))
+
+#%% Scene builder
+# Input: meshes for the objects + object coordinates in world coordinate system + cameraS(!) position (lights to be added)
+# Functions: load meshes, specify materials (later), specify cameras and their params, create the scene
+# Output: a scene dataclass containing all information to render the scene + an option to save the scene dataclass to file for later use
+
+# %% SIM-DATA MESH SET-UP
+# Read in the mesh data - for now from the sample SIMDATA file
+
 import pyvale.dataset as dataset
 import pyvale.mooseherder as mh
 import pyvale.sensorsim as sens
 
-# %% SIM-DATA MESH SET-UP
-# Read in the mesh data - for now from the sample SIMDATA file
 data_path = dataset.render_simple_block_path() # Load sample data file with a simple rectangular block in 3D to test image rendering algorithm. Returns a file path to an exodus file
 sim_data = mh.ExodusReader(data_path).read_all_sim_data() # Convert the simulation output into a SimData object
 displacement_components = ("disp_x","disp_y", "disp_z")
@@ -29,4 +37,80 @@ render_mesh.set_pos(np.array([0.0, 0.0, 0]))
 connectivity = render_mesh.connectivity
 coords = render_mesh.coords
 node_coords = coords[connectivity,:3]
+node_coords_flat = node_coords.reshape(node_coords.shape[0],node_coords.shape[1]*node_coords.shape[2]) # Shape to have a 2D array that can be mapped to Eigen matrix. Size is (num_elements, num_nodes_per_element*3) for triangles
+#print(node_coords)
+#print(node_coords_flat) 
+
+from superfastcode import cpp_simdata_rows
+print(cpp_simdata_rows(node_coords_flat))
+
+'''
+@dataclass(slots=True)
+class Scene:
+    A dataclass to hold all information about a 3D scene to be rendered
+    # World data (image, viewport, camera)
+    image_width: int
+    image_height: int
+    camera_center: np.ndarray
+    camera_target: np.ndarray
+    angle_vertical_view: int
+    matrix_camera_to_world: np.ndarray = field(init=False)
+    matrix_world_to_camera: np.ndarray = field(init=False)
+    matrix_pixel_spacing: np.ndarray = field(init=False)
+    viewport_upper_left: np.ndarray = field(init=False)
+    pixel_00_center: np.ndarray = field(init=False)
+    pixel_centers: np.ndarray = field(init=False) # Array of pixel center coordinates (on the viewport) in the world coordinate system
+    # Mesh data (to be expanded later))
+    node_coords: np.ndarray
+
+
+    def __post_init__(self) -> None:
+        self.create_basis_matrices()
+        i_indices, j_indices = np.meshgrid(np.arange(self.image_width), np.arange(self.image_height), indexing='xy')
+        pixel_centers = (self.pixel_00_center[np.newaxis, np.newaxis, :] +
+                     i_indices[:, :, np.newaxis] * self.matrix_pixel_spacing[0] +
+                     j_indices[:, :, np.newaxis] * self.matrix_pixel_spacing[1])
+
+
+     def create_basis_matrices(self) -> None:
+       
+        Creates camera-to-world matrix.
+        
+        self.matrix_camera_to_world = np.zeros((4, 4))
+        basis_vector_forward, basis_vector_right, basis_vector_up, focal_length = self._compute_camera_basis_vectors()
+        self.matrix_camera_to_world[:, :3] = np.array(
+            [basis_vector_right, basis_vector_up, basis_vector_forward, self.camera_center])
+        self.matrix_camera_to_world[3] = np.array([0.0, 0.0, 0.0, 1.0])
+        self.matrix_world_to_camera = np.linalg.inv(self.matrix_camera_to_world)
+        self._create_viewport(basis_vector_forward, basis_vector_right, basis_vector_up, focal_length)
+        # return self.matrix_camera_to_world, self.matrix_world_to_camera
+
+    def _compute_camera_basis_vectors(self):  # camera_center = lookfrom, point_camera_target = lookat
+        Creates the camera basis vectors from the camera center and the point the camera is looking at.
+        basis_vector_forward = self.camera_center - self.point_camera_target
+        focal_length = np.sqrt(basis_vector_forward.dot(basis_vector_forward))
+        vector_view_up = np.array([0.0, 1.0,
+                                   0.0])  # View up vector orthogonal to basis_vector_right. Defines sideways tilt. Value can be changed, this is the default for the camera to be straight.
+        basis_vector_right = np.cross(vector_view_up, basis_vector_forward)
+        basis_vector_up = np.cross(basis_vector_forward, basis_vector_right)
+        return normalise_vector(basis_vector_forward), normalise_vector(basis_vector_right), normalise_vector(
+            basis_vector_up), focal_length
+
+    def _create_viewport(self, basis_vector_forward, basis_vector_right, basis_vector_up, focal_length) -> None:
+        #Creates the viewport from the camera basis vectors and the focal length.
+        Returns pixel spacing vectors and the 0,0-positions for the pixel and the upper left corner of the viewport.
+        h_temp = tan(self.angle_vertical_view / 2)
+        viewport_height = 2 * h_temp * focal_length  # world units (arbitrary)
+        viewport_width = viewport_height * (image_width / image_height)  # world units (arbitrary)
+        # Viewport basis vectors
+        vector_viewport_x_axis = viewport_width * basis_vector_right  # Vu
+        vector_viewport_y_axis = (-viewport_height) * basis_vector_up  # Vw
+        # Pixel spacing vectors (delta vectors)
+        vector_pixel_spacing_x = vector_viewport_x_axis / image_width  # Delta u
+        vector_pixel_spacing_y = vector_viewport_y_axis / image_height  # Delta v
+        self.matrix_pixel_spacing = np.array([vector_pixel_spacing_x, vector_pixel_spacing_y])  # Store as an array
+        # 0,0-positions
+        self.viewport_upper_left = self.camera_center - basis_vector_forward - vector_viewport_x_axis / 2 - vector_viewport_y_axis / 2
+        self.pixel_00_center = self.viewport_upper_left + 0.5 * (vector_pixel_spacing_x + vector_pixel_spacing_y)
+
 '''
--- superfastcode/test1.cpp
+++ superfastcode/test1.cpp
@@ -2,15 +2,12 @@
 #include <pybind11/eigen.h>
 #include <pybind11/numpy.h>
 #include <Windows.h>
+#define _USE_MATH_DEFINES
 #include <cmath>
 #include "./Eigen/Dense"
 #include <cstdint>
+#include <corecrt_math_defines.h> // Otherwise M_PI doesn't work in VS. cmath alon works in CLion, though.
 
-// Struct for retrieving array dimensions from Python
-struct arrayDimensions {
-    uint32_t rows;
-    uint32_t cols;
-};
 
 double degreesToRadians(double angleDeg) {
     // Converts degrees to radians. Used to convert the angle of vertical view.
@@ -20,15 +17,15 @@ double degreesToRadians(double angleDeg) {
 // Aliases for the commonly used Eigen types with row-major storage order
 using EiMatrix4d = Eigen::Matrix<double, 4, 4, Eigen::StorageOptions::RowMajor>; // 4x4 matrix
 using EiVector3d = Eigen::Matrix<double, 1, 3, Eigen::StorageOptions::RowMajor>; // row vector (3D)
-using RmMatrixXd = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>; // row-major dynamic-size matrix, since MatrixxD is column-major by default
+using RmMatrixXd = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>; // row-major dynamic-size matrix, since MatrixXd                                                                                                                                     cbvcvbnx
+is column-major by default
 
 namespace py = pybind11;
 
 
 
-void getSimData(Eigen::Ref<RmMatrixXd> matrix) {
-    arrayDimensions dims{ matrix.rows(), matrix.cols() }; // verify that the return type matches what I assigned to the struct
-
+int getSimData(Eigen::Ref<RmMatrixXd> node_coords) {
+    return node_coords.rows();
 }
 
 
@@ -47,5 +44,5 @@ py::array_t<int> make_array(int nrows, int ncols) {
 }
 
 PYBIND11_MODULE(superfastcode, a) {
-    a.def("array_code", &make_array);
+    a.def("cpp_simdata_rows", &getSimData);
 }
